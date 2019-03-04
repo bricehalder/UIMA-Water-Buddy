2bd2d1492f604ac1eb001e30f43f49eb010b211f
Brice Halder <bhalder2@jhu.edu>
2019-03-04 18:51:32 -0500

    redo

---
+++ app/src/main/java/com/example/waterbuddy/SettingsActivity.java
@@ -0,0 +1,47 @@
+package com.example.waterbuddy;
+
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+        final EditText chooseTime = findViewById(R.id.daily_reset_input);
+
+        chooseTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TimePickerDialog timePickerDialog = new TimePickerDialog(SettingsActivity.this, R.style.TimePickerTheme,
+                        new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                        chooseTime.setText(String.format("%02d:%02d", hourOfDay, minutes));
+                    }
+                }, 0, 0, false);
+
+                timePickerDialog.show();
+
+
+            }
+        });
+
+
+
+    }
+
+}
